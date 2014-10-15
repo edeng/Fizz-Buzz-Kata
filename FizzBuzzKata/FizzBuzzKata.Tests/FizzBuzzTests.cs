4a900aac11e0745c3a4523c6e09b5b71fe141e58
unknown <edeng@GX790-025.paraport.com>
2014-10-15 15:07:36 -0700

    initial commit

---
+++ FizzBuzzKata/FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void getFizzMod3()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
